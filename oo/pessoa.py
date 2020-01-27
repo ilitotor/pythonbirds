2371749afb4e599e61304a4bd7971f73ea06ce03
Ilito Torquato <ilitotor@gmail.com>
2020-01-27 15:12:25 -0800

    Criado método cumprimentar e attr de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,12 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=38):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return f"olá {self.nome} com a idade de {self.idade}"
+
+if __name__ == '__main__':
+    p = Pessoa(nome="jose")
+    print (p.cumprimentar())
+    print(id(p.cumprimentar()))
